0aa6b0b6c64d006b3615cf4e5f98219ecbe9cb8c
Grant Liu <grant.liux@gmail.com>
2022-08-20 16:27:37 +1200

    Extracted project items into componenet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from 'react'
+import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
+import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills";
-
-
 
 export default function App() {
   return (
-    <div >
+    <div>
       <Navbar />
       <Home />
       <About />
       <Skills />
       <Projects />
       <Contact />
-
     </div>
   );
 }
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import arcade from "../../assets/arcade.png";
+import zapp from "../../assets/zapp.png";
+import portfolio from "../../assets/portfolio.png";
+import { RiArrowDownSLine } from "react-icons/ri";
+import { RiArrowUpSLine } from "react-icons/ri";
+import Club from "../../assets/club.png";
+import ProjectItem from "./ProjectItem";
+
+import { Link } from "react-scroll";
+
+export default function Projects() {
+  return (
+    <div
+      name="projects"
+      className="w-full min-h-screen text-primary bg-white relative snap-start"
+    >
+      <div className=" hidden sm:block bottom-0 right-0 opacity-20 m-0 p-0 absolute ">
+        <img src={Club} className="h-screen" alt="club background" />
+      </div>
+      <div className="max-w-[1000px] p-4 flex flex-col justify-center w-full h-full  absolute top-0 left-0 right-0 mx-auto">
+        <div>
+          <button className="text-primary py-3 my-2 flex items-center hover:scale-150 duration-200 ">
+            <Link to="skills" smooth={true} duration={500}>
+              <RiArrowUpSLine size={50} />
+            </Link>
+          </button>
+        </div>
+        <div className="pb-8">
+          <p className="text-5xl font-bold inline "> Projects</p>
+          <p className="py-6">Check out some of my favourite projects</p>
+        </div>
+        {/* Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <ProjectItem
+            title="Zapp"
+            image={zapp}
+            link="https://github.com/un-fathom/zapp"
+          />
+          <ProjectItem
+            title="Arcade Platformer"
+            image={arcade}
+            link="https://github.com/grantliunz/arcade-platformer"
+          />
+          <ProjectItem
+            title="Portfolio Website"
+            image={portfolio}
+            link="https://github.com/grantliunz/grantliu.com"
+          />
+        </div>
+        <div className="">
+          <Link to="contact" smooth={true} duration={500}>
+            <button className="text-primary py-3 my-2 flex items-center hover:scale-150 duration-200 ">
+              <RiArrowDownSLine size={50} />
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
