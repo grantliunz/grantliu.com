30a2f8438a8aa68d659650756d6579ff70c005f9
Grant Liu <grant.liux@gmail.com>
2022-08-20 15:25:00 +1200

    added project

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,46 +1,73 @@
-import React, { useEffect } from 'react'
-import { RiArrowUpSLine } from 'react-icons/ri'
-import { Link } from 'react-scroll'
-import kwesforms from 'kwesforms';
-
+import React, { useEffect } from "react";
+import { RiArrowUpSLine } from "react-icons/ri";
+import { Link } from "react-scroll";
+import kwesforms from "kwesforms";
 
 export default function Contact() {
-    useEffect(() => {
-        kwesforms.init();
-
-    }, []);
-    return (
-        <div name='contact' className=' w-full h-screen bg-primary text-white flex justify-center items-center p-4 snap-start' >
-            <div className='max-w-[1000px] p-4 flex flex-col justify-center w-full h-full mx-auto'>
-                <div>
-                    <button className='text-white py-3 my-2 flex items-center hover:scale-150 duration-200 '>
-                        <Link to="projects" smooth={true} duration={500}>
-                            <RiArrowUpSLine size={50} />
-                        </Link>
-                    </button>
-                </div>
-                <div className='pb-2'>
-                    <p className='text-5xl font-bold inline '>Contact</p>
-                    <p className='pt-4'>Got a question or suggestion?</p>
-                    <p className='pb-4'>Submit the form below to get in touch</p>
-                </div>
-
-                <form class="kwes-form" action="https://kwesforms.com/api/foreign/forms/aZYp8wflI0LBvZtqRo4F ">
-                    <div className='flex '>
-                        <div className='flex flex-col w-1/2 mr-2'>
-                            <input className=' w-full p-2 mr-1 mb-2   border-b-2  bg-transparent	contact-form' type="text" placeholder='Name' name='name' data-kw-rules="required|min:2|max:255" />
-                        </div>
-                        <div className='flex flex-col w-full'>
-                            <input className='p-2 ml-1 mb-2  w-full  border-b-2  bg-transparent contact-form' type="email" placeholder='Email' name='email' data-kw-rules="required|email" />
-                        </div>
-                    </div>
-
-                    <textarea className=' p-2 w-full border-b-2 bg-transparent contact-form' name="message" rows="1" placeholder='Message' data-kw-rules="required"></textarea>
-                    <button className='items-center cursor-pointer border-2 my-4 mx-auto px-3 py-2 rounded hover:bg-white group duration-200 block' type="submit" >
-                        <span className=' group-hover:text-primary duration-200 '>Submit</span>
-                    </button>
-                </form>
-            </div>
+  useEffect(() => {
+    kwesforms.init();
+  }, []);
+  return (
+    <div
+      name="contact"
+      className=" w-full h-screen bg-primary text-white flex justify-center items-center p-4 snap-start"
+    >
+      <div className="max-w-[1000px] p-4 flex flex-col justify-center w-full h-full mx-auto">
+        <div>
+          <button className="text-white py-3 my-2 flex items-center hover:scale-150 duration-200 ">
+            <Link to="projects" smooth={true} duration={500}>
+              <RiArrowUpSLine size={50} />
+            </Link>
+          </button>
         </div>
-    )
+        <div className="pb-2">
+          <p className="text-5xl font-bold inline ">Contact</p>
+          <p className="pt-4">Get in touch with me!!</p>
+          <p className="pb-4">Submit the form below to get in touch</p>
+        </div>
+
+        <form
+          class="kwes-form"
+          action="https://kwesforms.com/api/foreign/forms/aZYp8wflI0LBvZtqRo4F "
+        >
+          <div className="flex ">
+            <div className="flex flex-col w-1/2 mr-2">
+              <input
+                className=" w-full p-2 mr-1 mb-2   border-b-2  bg-transparent	contact-form"
+                type="text"
+                placeholder="Name"
+                name="name"
+                data-kw-rules="required|min:2|max:255"
+              />
+            </div>
+            <div className="flex flex-col w-full">
+              <input
+                className="p-2 ml-1 mb-2  w-full  border-b-2  bg-transparent contact-form"
+                type="email"
+                placeholder="Email"
+                name="email"
+                data-kw-rules="required|email"
+              />
+            </div>
+          </div>
+
+          <textarea
+            className=" p-2 w-full border-b-2 bg-transparent contact-form"
+            name="message"
+            rows="1"
+            placeholder="Message"
+            data-kw-rules="required"
+          ></textarea>
+          <button
+            className="items-center cursor-pointer border-2 my-4 mx-auto px-3 py-2 rounded hover:bg-white group duration-200 block"
+            type="submit"
+          >
+            <span className=" group-hover:text-primary duration-200 ">
+              Submit
+            </span>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,84 +1,119 @@
-import React from 'react'
-import pythonArcade from '../assets/arcade.png'
-import portfolio from "../assets/portfolio.png"
-import { RiArrowDownSLine } from 'react-icons/ri'
-import { RiArrowUpSLine } from 'react-icons/ri'
-import Club from '../assets/club.png'
+import React from "react";
+import pythonArcade from "../assets/arcade.png";
+import portfolio from "../assets/portfolio.png";
+import zapp from "../assets/zapp.png";
+import { RiArrowDownSLine } from "react-icons/ri";
+import { RiArrowUpSLine } from "react-icons/ri";
+import Club from "../assets/club.png";
 
-
-import { Link } from 'react-scroll'
+import { Link } from "react-scroll";
 
 export default function Projects() {
-    return (
-        <div name='projects' className='w-full h-screen text-primary bg-white relative snap-start'>
-            <div className=' hidden sm:block bottom-0 right-0 opacity-20 m-0 p-0 absolute '>
-                <img src={Club} className="h-screen" alt="club background" />
-            </div>
-            <div className='max-w-[1000px] p-4 flex flex-col justify-center w-full h-full  absolute top-0 left-0 right-0 mx-auto'>
-                <div>
-                    <button className='text-primary py-3 my-2 flex items-center hover:scale-150 duration-200 '>
-                        <Link to="skills" smooth={true} duration={500}>
-                            <RiArrowUpSLine size={50} />
-                        </Link>
-                    </button>
-                </div>
-                <div className='pb-8'>
-                    <p className='text-5xl font-bold inline ' > Projects</p>
-                    <p className='py-6'>Check out some of my projects</p>
-                </div>
-                {/* Container */}
-                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                    {/* Grid item */}
-                    <div style={{ backgroundImage: `url(${pythonArcade})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '>
-
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100 '>
-                            <span className='text-2xl fond-bold text-white tracking-wider'>
-                                Arcade Platformer
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href="https://github.com/grantliunz/arcade-platformer" target="_blank" rel="noreferrer" className=''>
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>
-                                        See More
-                                    </button>
-                                </a>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div style={{ backgroundImage: `url(${portfolio})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '>
-
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100 '>
-                            <span className='text-2xl fond-bold text-white tracking-wider'>
-                                grantliu.com
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href="https://github.com/grantliunz/grantliu.com" target="_blank" rel="noreferrer" className=''>
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>
-                                        See More
-                                    </button>
-                                </a>
-
-                            </div>
-                        </div>
-                    </div>
-
-
-
-
-                </div>
-                <div className=''>
-                    <Link to="contact" smooth={true} duration={500}>
-                        <button className='text-primary py-3 my-2 flex items-center hover:scale-150 duration-200 '>
-                            <RiArrowDownSLine size={50} />
-                        </button>
-                    </Link>
-
-                </div>
-
-
-            </div>
+  return (
+    <div
+      name="projects"
+      className="w-full h-screen text-primary bg-white relative snap-start"
+    >
+      <div className=" hidden sm:block bottom-0 right-0 opacity-20 m-0 p-0 absolute ">
+        <img src={Club} className="h-screen" alt="club background" />
+      </div>
+      <div className="max-w-[1000px] p-4 flex flex-col justify-center w-full h-full  absolute top-0 left-0 right-0 mx-auto">
+        <div>
+          <button className="text-primary py-3 my-2 flex items-center hover:scale-150 duration-200 ">
+            <Link to="skills" smooth={true} duration={500}>
+              <RiArrowUpSLine size={50} />
+            </Link>
+          </button>
         </div>
-    )
+        <div className="pb-8">
+          <p className="text-5xl font-bold inline "> Projects</p>
+          <p className="py-6">Check out some of my favourite projects</p>
+        </div>
+        {/* Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <div
+            style={{ backgroundImage: `url(${zapp})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div "
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 text-center">
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                Zapp
+              </span>
+
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/grantliunz/grantliu.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className=""
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    See More
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid item */}
+          <div
+            style={{ backgroundImage: `url(${pythonArcade})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div "
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 ">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Arcade Platformer
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/grantliunz/arcade-platformer"
+                  target="_blank"
+                  rel="noreferrer"
+                  className=""
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    See More
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid item */}
+          <div
+            style={{ backgroundImage: `url(${portfolio})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div "
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 ">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                grantliu.com
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/grantliunz/grantliu.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className=""
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    See More
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="">
+          <Link to="contact" smooth={true} duration={500}>
+            <button className="text-primary py-3 my-2 flex items-center hover:scale-150 duration-200 ">
+              <RiArrowDownSLine size={50} />
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
